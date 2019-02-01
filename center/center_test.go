10735e8e65cf6e6954fcf8e5bd8983d18a0d520e
kevin <wei.liu@ifchange.com>
2019-02-01 18:28:09 +0800

    fix

--- center/center_test.go
+++ center/center_test.go
@@ -29,3 +29,9 @@ func TestRun(t *testing.T) {
 	f.AddAction("/db/write", f.writeDB)
 	f.Run()
 }
+
+func TestFlight_WDB(t *testing.T) {
+	sql := "insert into `test` (`name`,`age`) values (?,?)"
+	f := &Flight{}
+	f.FlightDB.DB.Exec(sql, "Davie", 30)
+}
